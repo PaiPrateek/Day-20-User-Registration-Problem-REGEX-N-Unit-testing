892fc3ad45815dc8131996b4e45dccbc4cb23056
Prateek Pai <paiprateek123@gmail.com>
2022-07-10 08:54:12 +0530

    Validation for E-Mail

--- UserRegistrationProblem_RegexTesting/Program.cs
+++ UserRegistrationProblem_RegexTesting/Program.cs
@@ -17,6 +17,9 @@ namespace UserRegistrationProblem_RegexTesting
 
             //Validation for Last Name
             P.Validate_LastName("Pai");
+
+            //Validation for Email
+            P.Validate_Email("prateek.pai@gmail.co.in");
         }
 
         public bool Validate_FirstName(string firstName)
@@ -33,6 +36,14 @@ namespace UserRegistrationProblem_RegexTesting
             string LastNamePattern = @"^[A-Z]{1}[a-zA-Z]{2,}$";
 
             return Regex.IsMatch(lastName, LastNamePattern);
+        }
+
+        public bool Validate_Email(string email)
+        {
+            //Email Pattern 
+            string EmailPattern = @"^([A-Za-z]{3,}([.a-z]*)[@][a-z]{2,}[.][a-z]{2,3}([.a-z]*))$";
+
+            return Regex.IsMatch(email, EmailPattern);
         }
     }
 }
