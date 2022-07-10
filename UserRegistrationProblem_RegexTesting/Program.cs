23f9e5c07de28a1b9a82b863922c540d24bb8f25
Prateek Pai <paiprateek123@gmail.com>
2022-07-10 07:49:42 +0530

    Welcome to user Registration Problem - Regex Unit Testing

---
+++ UserRegistrationProblem_RegexTesting/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UserRegistrationProblem_RegexTesting
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to User Registration Problem - Regex Unit Testing");
+        }
+    }
+}
